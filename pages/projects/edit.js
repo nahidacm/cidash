b6bf6d3862b709744a720a6d20bca41185c75f31
nahid <comeback>
2022-07-14 18:24:17 +0600

    Seperated the project form as component

--- pages/projects/edit.js
+++ pages/projects/edit.js
@@ -1,29 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import { Button, Form, Input, message, Col, Row, Space } from "antd";
-import { MinusCircleOutlined, PlusOutlined } from "@ant-design/icons";
-const layout = {
-  labelCol: {
-    span: 8,
-  },
-  wrapperCol: {
-    span: 16,
-  },
-};
 
-/* eslint-disable no-template-curly-in-string */
-
-const validateMessages = {
-  required: "${label} is required!",
-  types: {
-    email: "${label} is not a valid email!",
-    number: "${label} is not a valid number!",
-  },
-  number: {
-    range: "${label} must be between ${min} and ${max}",
-  },
-};
-/* eslint-enable no-template-curly-in-string */
+import ProjectForm from "../../components/ProjectForm";
 
 export default function EditProject() {
   const router = useRouter();
@@ -75,88 +54,7 @@ export default function EditProject() {
   return (
     <Row justify="center">
       <Col xs={24} sm={24} md={18} lg={16} xl={12}>
-        <Form
-          {...layout}
-          name="project-form"
-          onFinish={onFinish}
-          validateMessages={validateMessages}
-          form={form}
-        >
-          <Form.Item
-            name="name"
-            label="Name"
-            rules={[
-              {
-                required: true,
-              },
-            ]}
-          >
-            <Input />
-          </Form.Item>
-
-          <Form.Item name="description" label="Description">
-            <Input.TextArea />
-          </Form.Item>
-
-          <Form.List name="steps">
-            {(fields, { add, remove }) => (
-              <>
-                {fields.map(({ key, name, ...restField }) => (
-                  <Space
-                    key={key}
-                    style={{
-                      display: "flex",
-                      marginBottom: 8,
-                    }}
-                    align="baseline"
-                  >
-                    <Form.Item
-                      {...restField}
-                      name={[name, "step_title"]}
-                      rules={[
-                        {
-                          required: true,
-                          message: "Missing step title",
-                        },
-                      ]}
-                    >
-                      <Input placeholder="Step Title" />
-                    </Form.Item>
-                    <Form.Item
-                      {...restField}
-                      name={[name, "command"]}
-                      rules={[
-                        {
-                          required: true,
-                          message: "Missing command",
-                        },
-                      ]}
-                    >
-                      <Input placeholder="Command" />
-                    </Form.Item>
-                    <MinusCircleOutlined onClick={() => remove(name)} />
-                  </Space>
-                ))}
-                <Form.Item>
-                  <Button
-                    type="dashed"
-                    onClick={() => add()}
-                    block
-                    icon={<PlusOutlined />}
-                  >
-                    Add Step
-                  </Button>
-                </Form.Item>
-              </>
-            )}
-          </Form.List>
-
-          <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
-            <Button type="primary" htmlType="submit">
-              Submit
-            </Button>
-          </Form.Item>
-        </Form>
+        <ProjectForm form={form} onFinish={onFinish} />
       </Col>
     </Row>
   );
--- pages/projects/new.js
+++ pages/projects/new.js
@@ -1,31 +1,12 @@
 import React from "react";
 import { Button, Form, Input, message, Col, Row, Space } from "antd";
-import { MinusCircleOutlined, PlusOutlined } from "@ant-design/icons";
-const layout = {
-  labelCol: {
-    span: 8,
-  },
-  wrapperCol: {
-    span: 16,
-  },
-};
+import ProjectForm from "../../components/ProjectForm";
+
 
 /* eslint-disable no-template-curly-in-string */
 
-const validateMessages = {
-  required: "${label} is required!",
-  types: {
-    email: "${label} is not a valid email!",
-    number: "${label} is not a valid number!",
-  },
-  number: {
-    range: "${label} must be between ${min} and ${max}",
-  },
-};
-/* eslint-enable no-template-curly-in-string */
-
 export default function NewProject() {
-  
+
   const addProject = async (values) => {
     const key = "updatable";
     message.loading({
@@ -52,87 +33,7 @@ export default function NewProject() {
   return (
     <Row justify="center">
       <Col xs={24} sm={24} md={18} lg={16} xl={12}>
-        <Form
-          {...layout}
-          name="nest-messages"
-          onFinish={onFinish}
-          validateMessages={validateMessages}
-        >
-          <Form.Item
-            name="name"
-            label="Name"
-            rules={[
-              {
-                required: true,
-              },
-            ]}
-          >
-            <Input />
-          </Form.Item>
-
-          <Form.Item name="description" label="Description">
-            <Input.TextArea />
-          </Form.Item>
-
-          <Form.List name="steps">
-            {(fields, { add, remove }) => (
-              <>
-                {fields.map(({ key, name, ...restField }) => (
-                  <Space
-                    key={key}
-                    style={{
-                      display: "flex",
-                      marginBottom: 8,
-                    }}
-                    align="baseline"
-                  >
-                    <Form.Item
-                      {...restField}
-                      name={[name, "step_title"]}
-                      rules={[
-                        {
-                          required: true,
-                          message: "Missing step title",
-                        },
-                      ]}
-                    >
-                      <Input placeholder="Step Title" />
-                    </Form.Item>
-                    <Form.Item
-                      {...restField}
-                      name={[name, "command"]}
-                      rules={[
-                        {
-                          required: true,
-                          message: "Missing command",
-                        },
-                      ]}
-                    >
-                      <Input placeholder="Command" />
-                    </Form.Item>
-                    <MinusCircleOutlined onClick={() => remove(name)} />
-                  </Space>
-                ))}
-                <Form.Item>
-                  <Button
-                    type="dashed"
-                    onClick={() => add()}
-                    block
-                    icon={<PlusOutlined />}
-                  >
-                    Add Step
-                  </Button>
-                </Form.Item>
-              </>
-            )}
-          </Form.List>
-
-          <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
-            <Button type="primary" htmlType="submit">
-              Submit
-            </Button>
-          </Form.Item>
-        </Form>
+        <ProjectForm onFinish={onFinish} />
       </Col>
     </Row>
   );
