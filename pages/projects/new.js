f17115ac313a87340a33e8eaab89e23e61d99a70
Raiyad Raad <raiyad.raad@sslwireless.com>
2022-07-18 12:52:21 +0600

    RND-1347: UI update

--- pages/projects/new.js
+++ pages/projects/new.js
@@ -2,39 +2,53 @@ import React from "react";
 import { Button, Form, Input, message, Col, Row, Space } from "antd";
 import ProjectForm from "../../components/ProjectForm";
 
-
-/* eslint-disable no-template-curly-in-string */
-
 export default function NewProject() {
+    // Antd Constants
+    const [form] = Form.useForm();
 
-  const addProject = async (values) => {
-    const key = "updatable";
-    message.loading({
-      content: "Saving...",
-      key,
-    });
-    const res = await fetch(process.env.NEXT_PUBLIC_BASE_URL+"/api/projects", {
-      method: "POST",
-      body: JSON.stringify(values),
-    });
+    const addProject = async (values) => {
+        const key = "updatable";
+        message.loading({
+            content: "Saving...",
+            key,
+        });
 
-    message.success({
-      content: "Saved Successfully!",
-      key,
-      duration: 2,
-    });
-  };
+        const res = await fetch(
+            process.env.NEXT_PUBLIC_BASE_URL + "/api/projects",
+            {
+                method: "POST",
+                body: JSON.stringify(values),
+            }
+        );
 
-  const onFinish = (values) => {
-    addProject(values);
-    console.log(values);
-  };
+        return new Promise((resolve, reject) => {
+            if (res && res?.status === 201) {
+                message.success({
+                    content: "Saved Successfully!",
+                    key,
+                    duration: 2,
+                });
+                resolve("success");
+            } else {
+                message.error({
+                    content: "Error saving data!",
+                    key,
+                    duration: 2,
+                });
+                reject("failed");
+            }
+        });
+    };
 
-  return (
-    <Row justify="center">
-      <Col xs={24} sm={24} md={18} lg={16} xl={12}>
-        <ProjectForm onFinish={onFinish} />
-      </Col>
-    </Row>
-  );
+    return (
+        <Row justify="center">
+            <Col xs={24} sm={24} md={18} lg={16} xl={12}>
+                <ProjectForm
+                    submitFunction={addProject}
+                    form={form}
+                    actionType="create"
+                />
+            </Col>
+        </Row>
+    );
 }
